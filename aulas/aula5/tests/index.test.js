ff6b3ac7e4c5aa570474827ebe305d7794e1a530
Julia <juliadsouz2@gmail.com>
2025-08-29 10:39:11 -0300

    Códigos da Aula 5

--- aulas/aula5/tests/index.test.js
+++ aulas/aula5/tests/index.test.js
@@ -1,3 +1,67 @@
+const calculadora = require('../src/index.js')
+
+describe("Teste de calculadora", () => {
 test ("2 + 2 = 4", () => {
-    expect (2 + 2).toBe (4)
+    expect (calculadora.soma).toBeDefined ()
+    expect (calculadora.soma(2,2)).toBe(4)
+})
+
+test ("2 + 0 = 2", () => {
+    expect (calculadora.soma(2,0)).toBe(2)
+})
+
+test ("-2 + -2 = -4", () => {
+    expect (calculadora.soma(-2,-2)).toBe(-4)
+})
+
+test ("2 - 2 = 0", () => {
+    expect (calculadora.subtracao).toBeDefined ()
+    expect (calculadora.subtracao(2,2)).toBe(0)
+})
+
+test ("-4 - 2 = -6", () => {
+    expect (calculadora.subtracao).toBeDefined ()
+    expect (calculadora.subtracao(-4, 2)).toBe(-6)
+})
+
+test("Se a >= b então a - b >= 0", () => {
+    expect (calculadora.subtracao(2,1)).toBeGreaterThanOrEqual(0);
+    expect (calculadora.subtracao(2,2)).toBeGreaterThanOrEqual(0);
+    expect (calculadora.subtracao(2,-2)).toBeGreaterThanOrEqual(0);
+    expect (calculadora.subtracao(-2,-4)).toBeGreaterThanOrEqual(0);
+    
+});
+
+test("Se a < b então a - b < 0", () => {
+    expect (calculadora.subtracao(1,2)).toBeLessThan(0);
+    expect (calculadora.subtracao(-2,1)).toBeLessThan(0);
+    expect (calculadora.subtracao(-2,-1)).toBeLessThan(0);
+    
+});
+
+
+test("Se a e b < 0 ou a e b > 0 então a * b > 0", () => {
+    expect (calculadora.subtracao).toBeDefined ()
+    expect (calculadora.multiplicacao(-3,-2)).toBeGreaterThan(0);
+    expect (calculadora.multiplicacao(2,1)).toBeGreaterThan(0);
+})
+
+test("Se a < 0 ou b < 0 então a * b < 0", () => {
+    expect (calculadora.multiplicacao(-3,2)).toBeLessThan(0);
+    expect (calculadora.multiplicacao(2,-1)).toBeLessThan(0);
+       
+});
+
+test("Se a = 0 ou b = 0 então a * b = 0", () => {
+    expect (calculadora.multiplicacao(-3,0)).toBe(-0);
+    expect (calculadora.multiplicacao(0,1)).toBe(0);
+       
+});
+
+test("Se b = 0 então Divisão por 0", () => {
+    expect (calculadora.divisao).toBeDefined();
+    expect(() => calculadora.divisao(2,0)).toThrow("Divisão por ZERO")
+       
+});
+
 })

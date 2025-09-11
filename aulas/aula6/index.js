82662a9f846884cabd0b938e86d7d510c7f8802f
Julia <juliadsouz2@gmail.com>
2025-09-11 10:44:50 -0300

    Códigos aula 7

--- aulas/aula6/index.js
+++ aulas/aula6/index.js
@@ -1,13 +1,39 @@
 // 1. importar o framework
 const express = require("express");
 
+// 1. importar middleware de terceiros
+const cors = require("cors");
+
+const router = require('./router');
 //2. Criar uma instancia da aplicacão
 const app = express();
+
+//middleware embutido ou integrado
+app.use(express.json());
+//?param1=valor1&param2=valor2
+app.use(express.urlencoded({extended: false})); 
+
+// middleware de terceiros
+app.use(cors());
+
+//middleware de aplicação
+app.use((req,res,next) => {
+    console.log('Passei pelo middleware de app');
+    next();
+});
+
+app.use('/tarefas',router);
 
 //Criar um  middleware
 app.get('/',(req, res) => {
         res.send("Olá")
+
 });
+
+//middleware de erro
+app.use((err,req,res,next) => {
+    res.status(500).send(err,mesage);
+})
 
 //3. Iniciar a aplicacão de uma porta
 app.listen(3000, () => {
